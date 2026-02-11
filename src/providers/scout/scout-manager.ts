5c82afaadd5e80528ec16285501c869e9f977b45
Charles Teague <charles@merdianlabs.ai>
2026-02-11 09:33:30 -0500

    fix: Require Scout 0.4.13
    The VSCode extension now requires version 0.4.13 of Scout. This change properly disables the Scout support if an earlier version of Scout is present.

--- src/providers/scout/scout-manager.ts
+++ src/providers/scout/scout-manager.ts
@@ -1,8 +1,28 @@
 import { ExtensionContext } from "vscode";
-import { scoutBinPath } from "../../scout/props";
+import { scoutBinPath, scoutVersionDescriptor } from "../../scout/props";
 import { PackageManager } from "../../core/package/manager";
+import { SemVer } from "semver";
 
 // Activates the provider which tracks the availability of Scout
 export function activateScoutManager(context: ExtensionContext) {
-  return new PackageManager(context, "inspect_scout", scoutBinPath);
+  return new PackageManager(
+    context,
+    "inspect_scout",
+    scoutBinPath,
+    checkIsEnabled
+  );
 }
+
+const checkIsEnabled = () => {
+  const version = scoutVersionDescriptor();
+  if (!version) {
+    return false;
+  }
+  return supportsRouteMessages(version.version);
+};
+
+const supportsRouteMessages = (version: SemVer): boolean => {
+  // Whether the scout version supports route messaging, used in vscode
+  // extension version 0.9 and later
+  return version.major >= 0 && version.minor >= 4 && version.patch > 12;
+};
